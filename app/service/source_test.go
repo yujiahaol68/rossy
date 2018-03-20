8758366da6ebc4346e1365284ebb8b4aa8b81571
Tecker.Yu <tecker_yuknigh@163.com>
2018-03-20 20:39:00 +0800

    [test]: fix test out of range

--- app/service/source_test.go
+++ app/service/source_test.go
@@ -26,9 +26,9 @@ func TestMain(m *testing.M) {
 
 func Test_GetSourceById(t *testing.T) {
 	s := service.Source{database.Conn()}
-	source := s.FindOne(int64(250))
+	source := s.FindOne(int64(57))
 
-	if source.ID != int64(250) {
+	if source.ID != int64(57) {
 		t.Fatalf("expect source ID is 250, but got %v", source.ID)
 	}
 }
