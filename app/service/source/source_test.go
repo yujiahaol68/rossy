961d46341d75839c57b810dd93cc11cf6c1010dc
Tecker.Yu <tecker_yuknigh@163.com>
2018-03-21 21:27:47 +0800

    [test]: fix path error

--- app/service/source/source_test.go
+++ app/service/source/source_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/yujiahaol68/rossy/app/database"
-	"github.com/yujiahaol68/rossy/app/repo/source"
+	"github.com/yujiahaol68/rossy/app/service/source"
 )
 
 func setup() error {
